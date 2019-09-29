0a20b8dcda9381184e139fbb23a7a8b0f20ea971
Steve Jorgensen <stevej@cliosoft.com>
2019-09-29 07:08:45 +0000

    Test delegating both value get & method call

--- tests/test_delegates_calls.py
+++ tests/test_delegates_calls.py
@@ -1,20 +1,38 @@
 from delegation import DelegatesAttrs
 
 
-class InnerSubject:
+class Inner:
+    color = 'purple'
+
     def get_hello(self):
         return 'Hello from inner subject!'
 
 
-class WrapperSubject(DelegatesAttrs):
-    delegate('get_hello to inner_obj')
+def make_wrapper_class(body_lines):
+    code = f"""
+class Wrapper(DelegatesAttrs):
+{body_lines}
 
     def __init__(self, inner_obj):
         self.inner_obj = inner_obj
+"""
+    exec(code)
+    return eval('Wrapper')
 
 
-def test_something():
-    inner_subject = InnerSubject()
-    wrapper_subject = WrapperSubject(inner_subject)
+def test_delegated_value_get():
+    Wrapper = make_wrapper_class("""
+    delegate('color to inner_obj')
+""")
+    wrapper = Wrapper(Inner())
 
-    assert wrapper_subject.get_hello() == 'Hello from inner subject!'
+    assert wrapper.color == 'purple'
+
+
+def test_delegated_method_call():
+    Wrapper = make_wrapper_class("""
+    delegate('get_hello to inner_obj')
+""")
+    wrapper = Wrapper(Inner())
+
+    assert wrapper.get_hello() == 'Hello from inner subject!'
